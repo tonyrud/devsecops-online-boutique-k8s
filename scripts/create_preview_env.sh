23de4acabe2f2c8b24c4944dd68c42e4a1b993da
tonyrud <tonrudny@gmail.com>
2025-03-09 12:21:21 -0400

    test separate appset

--- scripts/create_preview_env.sh
+++ scripts/create_preview_env.sh
@@ -13,8 +13,14 @@ cp -R overlays/liveview-counter/dev/. $PREVIEW_DIR
 # update image tag
 yq -i "(.images[] | select(.name == \"phoenix-liveview-counter\") | .newTag) = \"$BRANCH_NAME\"" $PREVIEW_DIR/kustomization.yaml
 
+INGRESS_HOST="$BRANCH_NAME.tonyrudny.com"
+
 # update ingress host
-yq -i "(.[0] | select(.path == \"/spec/rules/0/host\") | .value) = \"$BRANCH_NAME.tonyrudny.com\"" $PREVIEW_DIR/ingress-patches.yaml
+yq -i "(.[0] | select(.path == \"/spec/rules/0/host\") | .value) = \"$INGRESS_HOST\"" $PREVIEW_DIR/ingress-patches.yaml
 
 # update namespace
 yq -i "(.namespace) = \"$BRANCH_NAME\"" $PREVIEW_DIR/kustomization.yaml
+
+# set ENVs
+# replaces dev.tonyrudny.com with $INGRESS_HOST
+sed -i '' "s/dev.tonyrudny.com/$INGRESS_HOST/g" $PREVIEW_DIR/.env
